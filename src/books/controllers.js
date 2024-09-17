97a1e8449ad07a67da97daa62e8224c0621f215f
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-09-17 15:09:28 +0100

    add create for addbook route

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,7 +1,15 @@
+const Book = require("./model");
+
 const addBook = async (req, res) => {
   try {
     // try the thing
-    res.status(201).json({ message: "success" });
+    const book = await Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+    });
+
+    res.status(201).json({ message: "success", book: book });
   } catch (error) {
     // if error then does something
     res.status(500).json({ message: error.message, error: error });
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const bookRouter = require("./books/routes");
 
+const Book = require("./books/model");
+
 const port = process.env.PORT || 5001;
 
 const app = express();
@@ -11,10 +13,15 @@ app.use(express.json());
 
 app.use(bookRouter);
 
+const syncTables = () => {
+  Book.sync();
+};
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "API is healthy" });
 });
 
 app.listen(port, () => {
+  syncTables();
   console.log(`Server is listening on port ${port}`);
 });
